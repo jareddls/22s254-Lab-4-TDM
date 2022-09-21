edb02f8efb69a652eb5b5194027eb60903bbb233
Jared De Los Santos <jayred_dls@csu.fullerton.edu>
2022-09-20 17:12:04 -0700

    Do this one Charles, and update.

--- print_readme.h
+++ print_readme.h
@@ -1,16 +1,2 @@
-#include <iostream>
-#include <string>
-#include "print_readme.h"
-
-std::string readme(std::ifstream &file_name) {
-	std::string str;
-	if (file_name.is_open()) {
-		std::ostringstream ss;
-		ss << file_name.rdbuf();
-		str = ss.str();
-		return str;
-	}
-	else {
-		return "Error: File is not opened.";
-	}
-}
+/* FOR CHARLES: Put the contents of the print_readme.h
+file here. get rid of this comment after you have done so.*/
